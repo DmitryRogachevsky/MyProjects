8717df09427bf178515a4127d2def998ba2efa2f
DmitryRogachevsky <115455954+DmitryRogachevsky@users.noreply.github.com>
2023-01-26 14:14:11 +0300

    Add files via upload

---
+++ Assessment.php
@@ -0,0 +1,17 @@
+<?php
+function getCornersCount(string...$shapeName){
+	foreach ($shapeName as $value) {
+		switch ($value) {
+			case "square":
+			echo "$value - 4\n";
+			break;
+			case "circle":
+			echo "$value - 0\n";
+			break;
+			default:
+			echo "not defined\n";
+		}
+	}
+}
+
+getCornersCount('square', 'circle', 'triangle');
